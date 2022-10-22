c9b85bcef8f17e1916154ba214736caabb37fc94
Win Chiu <win881206@gmail.com>
2022-10-22 20:48:14 +0800

    router bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,17 @@ import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import NavBarMin from './components/NavBarMin';
 import Activity from './pages/Activity';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <React.Fragment>
+    <BrowserRouter>
       <NavBar />
-      <Activity />
+      <Routes>
+        {/* <Route exact path="/" element={<></>} /> */}
+        <Route path="/activity" element={<Activity />} />
+      </Routes>
       <Footer />
-    </React.Fragment>
+    </BrowserRouter>
   );
 }
 
