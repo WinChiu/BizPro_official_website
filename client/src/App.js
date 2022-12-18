5a2cbe336690bf42789603fb89a681e2547d1411
Win Chiu <win881206@gmail.com>
2022-12-19 00:30:26 +0800

    heroku built

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { lazy } from 'react';
 import './css/style.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
-import NavBarMin from './components/NavBarMin';
 import Activity from './pages/Activity';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/style.css';
 import logo_red from '../asset/img/logo_red.svg';
 import icon_hamburger from '../asset/img/icon/icon_hamburger.svg';
-function NavBarMin() {
-  return (
-    <div className="navBar">
+import $ from 'jquery';
+
+function NavBar() {
+  const [dropDownOpen, setdropDownOpen] = useState(false);
+
+  const NavBarL = () => (
+    <div className="navBar navBarL">
       <img src={logo_red} alt="bizPro Logo" className="navBar__logo" />
       <div className="navBar__list">
         <div className="navBar__item navBar__word">
@@ -25,6 +29,54 @@ function NavBarMin() {
       </div>
     </div>
   );
+  const NavBarS = () => (
+    <div className="navBar navBarS">
+      <img src={logo_red} alt="bizPro Logo" className="navBar__logo" />
+      <div className="navBar__list">
+        <img
+          className="navBar__item navBar__hamburger"
+          src={icon_hamburger}
+          alt=""
+          onClick={() => {
+            if (dropDownOpen === false) {
+              setdropDownOpen(true);
+              setTimeout(() => {
+                $('.navBar__dropdown').css('display', 'flex');
+              }, 0);
+            } else {
+              setdropDownOpen(false);
+              setTimeout(() => {
+                $('.navBar__dropdown').css('display', 'none');
+              }, 0);
+            }
+          }}
+        />
+      </div>
+      <div className="navBar__dropdown">
+        <div className="dropdown__item">
+          <a href="/about">關於 BizPro</a>
+        </div>
+        <div className="dropdown__item">
+          <a href="/journey">學習旅程</a>
+        </div>
+        <div className="dropdown__item">
+          <a href="/members">歷屆成員</a>
+        </div>
+        <div className="dropdown__item">
+          <a href="/activity">社群活動</a>
+        </div>
+        <div className="dropdown__item">
+          <a href="/member_talk">成員心得</a>
+        </div>
+      </div>
+    </div>
+  );
+  return (
+    <React.Fragment>
+      <NavBarS />
+      <NavBarL />
+    </React.Fragment>
+  );
 }
 
-export default NavBarMin;
+export default NavBar;
