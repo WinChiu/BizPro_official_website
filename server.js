6e2940072427e6254c5544da5210b29e46274d97
Win Chiu <win881206@gmail.com>
2022-12-19 00:41:49 +0800

    heroku built

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+
+//app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static('client/build'));
 
 if (process.env.NODE_DEV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(
       path.resolve(__dirname.anchor, 'client', 'build', 'index.html')
     );
   });
 }
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
