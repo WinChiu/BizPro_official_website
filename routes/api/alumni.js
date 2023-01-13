9ff0ce5dc7af719f2c04324195078bdc99026cfe
yichunlo <adjadj86@gmail.com>
2023-01-13 16:09:43 +0800

    Add GET API

--- routes/api/alumni.js
+++ routes/api/alumni.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const config = require('config');
+//const config = require('config');
 
 const Alumni = require('../../models/Alumni');
 
-
+router.get('/members', async (res, req) => {
+    try{
+        const alumni = await Alumni.find({});
+        if(!alumni) {
+            return res.status(400).json({msg: 'No alumni data available'});
+        }
+        return res.json(alumni);
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
---
+++ routes/api/article.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const Article = require('../../models/Article');
+
+router.get('/member_talk', async (res, req) => {
+    try{
+        const article = await Article.find({});
+        if (!article){
+            return res.status(400).json({msg: 'Ariticle data is not available'});
+        }
+        return res.json(article);
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
