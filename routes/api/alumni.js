09206f3713e08dc08d25a968940dbb6ab64a80b9
Win Chiu <win881206@gmail.com>
2023-01-14 18:29:04 +0800

    heroku 0114

--- routes/api/alumni.js
+++ routes/api/alumni.js
@@ -5,15 +5,14 @@ const router = express.Router();
 const Alumni = require('../../models/Alumni');
 
 router.get('/members', async (res, req) => {
-    try{
-        const alumni = await Alumni.find({});
-        if(!alumni) {
-            return res.status(400).json({msg: 'No alumni data available'});
-        }
-        return res.json(alumni);
+  try {
+    const alumni = await Alumni.find({});
+    if (!alumni) {
+      return res.status(400).json({ msg: 'No alumni data available' });
     }
-    catch(err){
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
+    return res.json(alumni);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
