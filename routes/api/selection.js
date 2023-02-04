5ef11ff5a8757fd817ee04b4f6e8dcafe4c4e176
yichunlo <adjadj86@gmail.com>
2023-02-04 16:53:05 +0800

    modify query form

--- routes/api/selection.js
+++ routes/api/selection.js
@@ -2,17 +2,55 @@ const express = require('express');
 const router = express.Router();
 const Alumni = require('../../models/Alumni');
 
+function findQuery(req){
+    let search_all_number = false;
+    let search_all_major = false;
+    let search_all_tags = false;
+    if(req.body.number == '0')
+        search_all_number = true;
+    if(req.body.major.length == 0)
+        search_all_major = true;
+    if(req.body.tags.length == 0)
+        search_all_tags = true;
+    console.log("search all number:", search_all_number);
+    console.log("search all major:", search_all_major);
+    console.log("search all tags:", search_all_tags);
+
+    let query = {};
+    if(search_all_number && search_all_major && search_all_tags)
+        query = {};
+    else if(search_all_number && search_all_tags)
+        query = {"major": req.body.major};
+    else if(search_all_number && search_all_major)
+        query = {"tags": req.body.tags};
+    else if(search_all_major && search_all_tags){
+        query = {"number": req.body.number};
+        console.log("Success");
+    }
+    else if(search_all_number)
+        query = {"tags": req.body.tags, "major": req.body.major};
+    else if(search_all_major)
+        query = {"tags": req.body.tags, "number": req.body.number};
+    else if(search_all_tags)
+        query = {"number": req.body.number, "major": req.body.major};
+    else
+        query = {
+            "number": req.body.number,
+            "major": req.body.major,
+            "tags": req.body.tags
+        }
+    return query;
+}
+
 router.get('/select', async (req, res) => {
     try{
         //console.log(req.body);
         tags_length = req.body.tags.length;
         major_length = req.body.major.length;
         
-        const alumniData = await Alumni.find({
-            "number": req.body.number,
-            "major": req.body.major,
-            "tags": req.body.tags,
-        });
+        query = findQuery(req);
+        console.log(query);
+        const alumniData = await Alumni.find(query);
         if(!alumniData){
             res.status(400).json({ msg: 'No alumni data available' });
         }
