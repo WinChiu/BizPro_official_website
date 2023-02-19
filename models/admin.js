789ccf64936de6c5ba2e60bc382c61deb7a05ba9
yichunlo <adjadj86@gmail.com>
2023-02-19 13:09:30 +0800

    add admin model for later auth use

---
+++ models/admin.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const AdminSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        }
+    }, {collation: 'admin'}
+);
+
+module.exports = Admin = mongoose.model('admin', AdminSchema);
