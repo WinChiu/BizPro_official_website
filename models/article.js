82468dfdc3359c02828204f98a880f797033ba8a
yichunlo <adjadj86@gmail.com>
2023-02-19 17:28:09 +0800

    use article model and article collection instead of article_test

--- models/article.js
+++ models/article.js
@@ -2,27 +2,15 @@ const mongoose = require('mongoose');
 
 const ArticleSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
-    number: {
-      type: String,
-      required: true,
-    },
-    jobTitle: {
-      type: String,
-      required: true,
+    alumni: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'alumni',
     },
     title: {
       type: String,
     },
     content: {
       type: String,
-      required: true,
-    },
-    tags: {
-      type: [String],
       required: true,
     },
     avatar: {
