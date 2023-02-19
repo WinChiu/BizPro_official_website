5f86a6ded49866f5312fa0505cfab5f173a44813
yichunlo <adjadj86@gmail.com>
2023-02-19 14:20:07 +0800

    add update tags

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -4,7 +4,6 @@ const Alumni = require('../../models/alumni');
 const Article = require('../../models/article_test');
 const Admin = require('../../models/admin');
 const { check, validationResult } = require('express-validator');
-const { query } = require('express');
 
 router.post(
     '/add_alumni',
@@ -19,8 +18,7 @@ router.post(
             return res.status(400).json({ errors: errors.array() });
         }
         const {name, number, jobTitle, exp, tags, avatar, major} = req.body;
-        console.log(name);
-        console.log(number);
+   
         try{
             let alumni = await Alumni.findOne({ 
                 name, number
@@ -60,7 +58,6 @@ router.put(
     '/update',
     check('name', 'Name is required').notEmpty(),
     check('number', 'Number is required').notEmpty(),
-    //check('jobTitle', 'Job Title is required').notEmpty(),
     async (req, res) => {
         const error = validationResult(req);
         if(!error.isEmpty()){
@@ -72,20 +69,20 @@ router.put(
                 number: req.body.number
             };
 
-            console.log(req.body.jobTitle);
-
             let alumni = await Alumni.findOne(query);
             if(!alumni){
                 console.log('No alumni');
                 return res.status(400).json({msg: "No alumni data"});
             }
-            //console.log(alumni);
+
             if (req.body.jobTitle != "" && req.body.jobTitle != null)
                 alumni.jobTitle = req.body.jobTitle;
             if (req.body.exp != "" && req.body.exp != null)
                 alumni.exp = req.body.exp;
             if (req.body.avatar != "" && req.body.avatar != null)
                 alumni.avatar = req.body.avatar;
+            if (req.body.tags != "" && req.body.tags != null)
+                alumni.tags = req.body.tags;
             if (req.body.major != "" && req.body.major != null)
                 alumni.major = req.body.major;
             await alumni.save();
