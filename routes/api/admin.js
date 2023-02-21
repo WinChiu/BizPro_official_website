64fb6f9d88c04243d4981605506c7c52aab2bbd4
yichunlo <adjadj86@gmail.com>
2023-02-21 14:32:40 +0800

    merge admin

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -18,20 +18,17 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
     const { name, number, jobTitle, exp, tags, avatar, major } = req.body;
-
-    try {
-      let alumni = await Alumni.findOne({
-        name,
-        number,
+   
+    try{
+      let alumni = await Alumni.findOne({ 
+        name, number
       });
-      if (alumni) {
+      if (alumni){
         console.log(alumni);
-        return res
-          .status(400)
-          .json({ errors: [{ msg: 'alumni already exists' }] });
+        return res.status(400).json({errors: [{msg: 'alumni already exists'}]});
       }
-      // Todo: avatar normalization
 
+      // Todo: avatar normalization
       alumni = new Alumni({
         name,
         number,
@@ -41,9 +38,8 @@ router.post(
         avatar,
         major,
       });
-
       await alumni.save();
-      res.send('alumni added');
+      res.send('alumni added success!!');
 
       const payload = {
         alumni: {
@@ -57,43 +53,97 @@ router.post(
   }
 );
 
-router.put(
-  '/update',
-  check('name', 'Name is required').notEmpty(),
-  check('number', 'Number is required').notEmpty(),
-  async (req, res) => {
-    const error = validationResult(req);
-    if (!error.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+router.post(
+    '/add_article',
+    check('alumni', 'Alumni ID is required').notEmpty(),
+    check('Title', 'Title is required').notEmpty(),
+    check('content', 'Content is required').notEmpty(),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const {_id, title, content, avatar} = req.body;
+        try{
+            let query = {
+                _id,
+                title
+            };
+            let article = await Article.find(query);
+            if (article){
+                console.log(article);
+                console.log('Article already exists!');
+                return res.status(400).json({msg: "Article exists"});
+            }
+            article = new Article({
+                _id,
+                title,
+                content,
+                avatar
+            });
+            await article.save();
+            res.send("Article added success!!")
+        }
+        catch(e){
+            console.error(e.message);
+            return res.status(500).json({msg: "Server Errorrrr"});
+        }
     }
-    try {
-      const query = {
-        name: req.body.name,
-        number: req.body.number,
-      };
-
-      let alumni = await Alumni.findOne(query);
-      if (!alumni) {
-        console.log('No alumni');
-        return res.status(400).json({ msg: 'No alumni data' });
-      }
-
-      if (req.body.jobTitle != '' && req.body.jobTitle != null)
-        alumni.jobTitle = req.body.jobTitle;
-      if (req.body.exp != '' && req.body.exp != null) alumni.exp = req.body.exp;
-      if (req.body.avatar != '' && req.body.avatar != null)
-        alumni.avatar = req.body.avatar;
-      if (req.body.tags != '' && req.body.tags != null)
-        alumni.tags = req.body.tags;
-      if (req.body.major != '' && req.body.major != null)
-        alumni.major = req.body.major;
-      await alumni.save();
-      res.json(alumni);
-    } catch (e) {
-      console.error(e.message);
-      return res.status(400).json({ msg: 'Server Errrrrror' });
-    }
-  }
 );
+
+router.put(
+    '/update_alumni',
+    check('_id', "Alumni ID is required").notEmpty(),
+    //check('name', 'Name is required').notEmpty(),
+    //check('number', 'Number is required').notEmpty(),
+    async (req, res) => {
+        const error = validationResult(req);
+        if(!error.isEmpty()){
+            return res.status(400).json({ error: error.array() });
+        }
+        try{
+            //console.log('id_:', req.body._id);
+            const id = req.body._id;
+
+            let alumni = await Alumni.findById(id);
+            if(!alumni){
+                console.log('No alumni');
+                return res.status(400).json({msg: "No alumni data"});
+            }
+
+            if (req.body.jobTitle != "" && req.body.jobTitle != null)
+                alumni.jobTitle = req.body.jobTitle;
+            if (req.body.exp != "" && req.body.exp != null)
+                alumni.exp = req.body.exp;
+            if (req.body.avatar != "" && req.body.avatar != null)
+                alumni.avatar = req.body.avatar;
+            if (req.body.tags != "" && req.body.tags != null)
+                alumni.tags = req.body.tags;
+            if (req.body.major != "" && req.body.major != null)
+                alumni.major = req.body.major;
+            await alumni.save();
+            
+            res.json(alumni);
+        }
+        catch(e){
+            console.error(e.message);
+            return res.status(400).json({msg: "Server Errrrrror"});
+        }
+    }
+);
+/*
+router.delete(
+    '/erase',
+    async (req, res) => {
+        try{
+            Alumni.findOneAndRemove();
+        }
+        catch(e){
+            console.error(e.message);
+            res.status(500).send('Server Errrorororor');
+        }
+    }
+);
+*/
 
 module.exports = router;
