66d168bd71374d146e650dad68e8504340663c77
yichunlo <adjadj86@gmail.com>
2023-02-21 19:08:52 +0800

    fix article function (add & update)

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const Alumni = require('../../models/alumni');
@@ -57,34 +58,40 @@ router.post(
 );
 
 router.post(
-  '/add_article',
-  check('alumni', 'Alumni ID is required').notEmpty(),
-  check('Title', 'Title is required').notEmpty(),
+  '/add_and_update_article',
+  check('name', 'Alumni name is required').notEmpty(),
+  check('number', 'Alumni number is required').notEmpty(),
+  check('title', 'Title is required').notEmpty(),
   check('content', 'Content is required').notEmpty(),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    const { _id, title, content, avatar } = req.body;
+    const { title, content, avatar } = req.body;
     try {
-      let query = {
-        _id,
-        title,
-      };
-      let article = await Article.find(query);
-      if (article) {
-        console.log(article);
-        console.log('Article already exists!');
-        return res.status(400).json({ msg: 'Article exists' });
-      }
-      article = new Article({
-        _id,
-        title,
-        content,
-        avatar,
+      //  Search alumni ID
+      let alumni = await Alumni.findOne({
+        name: req.body.name,
+        number: req.body.number
       });
-      await article.save();
+
+      let alumniID = alumni._id;
+      let articleFields = {
+        "alumni": alumniID,
+        "title": req.body.title,
+        "content": req.body.content,
+        "avatar": req.body.avatar
+      }
+      
+      let article = await Article.findOneAndUpdate(
+        { "alumni": alumniID}, 
+        { $set: articleFields},
+      );
+      if(!article){
+        article = await Article.create(articleFields);
+      }
+      //console.log(article);
       res.send('Article added success!!');
     } catch (e) {
       console.error(e.message);
@@ -113,9 +120,14 @@ router.put(
         return res.status(400).json({ msg: 'No alumni data' });
       }
 
+      if (req.body.name != '' && req.body.name != null)
+        alumni.name = req.body.name;
+      if (req.body.number != '' && req.body.number != null)
+        alumni.number = req.body.number;
       if (req.body.jobTitle != '' && req.body.jobTitle != null)
         alumni.jobTitle = req.body.jobTitle;
-      if (req.body.exp != '' && req.body.exp != null) alumni.exp = req.body.exp;
+      if (req.body.exp != '' && req.body.exp != null) 
+        alumni.exp = req.body.exp;
       if (req.body.avatar != '' && req.body.avatar != null)
         alumni.avatar = req.body.avatar;
       if (req.body.tags != '' && req.body.tags != null)
@@ -154,6 +166,5 @@ router.delete(
         }
     }
 );
-
 
 module.exports = router;
