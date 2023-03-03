2f949c757a82b59e8a79438713ea142e26a929ca
Win Chiu <win881206@gmail.com>
2023-03-03 14:18:59 +0800

    0303

---
+++ sitemap-routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+export default (
+  <Route>
+    <Route path="/" />
+    <Route path="/member" />
+    <Route path="/member_talk" />
+    <Route path="/journey" />
+  </Route>
+);
