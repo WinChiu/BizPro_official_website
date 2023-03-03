d5562237f95cf94db4aed9483756f62704192e12
Win Chiu <win881206@gmail.com>
2023-03-03 17:02:56 +0800

    0303

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 var cors = require('cors');
 var app = express();
 const connectDB = require('./config/db');
-const PORT = process.env.PORT || 5000;
 
 connectDB();
 
@@ -23,11 +22,15 @@ app.use('/api/article', require('./routes/api/article'));
 app.use('/api/admin', require('./routes/api/admin'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-app.use(express.static(path.join(__dirname, 'client/build')));
 if (process.env.NODE_ENV === 'production') {
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
