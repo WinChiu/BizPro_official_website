1dda0897ef60bc553326c9df5e5594fbc66563d5
Win Chiu <bizpro.taipei@gmail.com>
2023-03-25 21:45:26 +0800

    safari issue

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var cors = require('cors');
 var app = express();
 const connectDB = require('./config/db');
 const sitemapRouter = require('./routes/sitemap');
+const helmet = require('helmet');
 
 connectDB();
 
@@ -14,9 +15,18 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
   res.header('Access-Control-Allow-Credentials', true);
-  res.setHeader('Content-Security-Policy', "worker-src 'self' blob:;");
   next();
 });
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      'default-src': ["'self'"],
+      'worker-src': ["'self'", 'blob:'],
+      'object-src': ["'none'"],
+    },
+  })
+);
 
 app.use('/api/alumni', require('./routes/api/alumni'));
 app.use('/api/alumni', require('./routes/api/selection'));
