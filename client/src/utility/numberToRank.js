6db5019a59d4118e6942378835d274079e58e4ac
Win Chiu <bizpro.taipei@gmail.com>
2023-03-26 15:31:12 +0800

    0326

--- client/src/utility/numberToRank.js
+++ client/src/utility/numberToRank.js
@@ -1,12 +1,10 @@
 export default function numberToRank(number) {
   if (number === '1') {
     return number + 'st';
-  } else if (number[number.length - 1] === '1') {
-    return number + 'st';
-  } else if (number[number.length - 1] === '2') {
-    return number + 'nd';
-  } else if (number[number.length - 1] === '3') {
+  } else if (number === '2') {
     return number + 'rd';
+  } else if (number === '3') {
+    return number + 'st';
   } else {
     return number + 'th';
   }
