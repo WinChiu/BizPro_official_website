fa0429b55d164ab9558d07723bf8deeceae10e03
Win Chiu <bizpro.taipei@gmail.com>
2023-03-26 13:36:29 +0800

    0326

--- client/src/App.js
+++ client/src/App.js
@@ -17,35 +17,9 @@ function App() {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   };
-  // window.addEventListener('resize', setVh);
-  // window.onload = () => {
-  //   setVh();
-  //   let nowLocation = window.location.href;
-  //   if (nowLocation[nowLocation.length - 1] === '/') {
-  //     $('.logoContainer').css('animation-iteration-count', '1');
-  //     console.log('stop loading');
-  //     setTimeout(() => {
-  //       $('.logoContainer').css('width', '200px');
-  //       console.log('show logo');
-
-  //       setTimeout(() => {
-  //         $('.logoLoading').fadeOut();
-  //         console.log('fadeout 1');
-
-  //         $('body').css('overflow-y', 'scroll');
-  //       }, 1200);
-  //     }, 1200);
-  //   } else {
-  //     $('.logoLoading').fadeOut();
-  //     console.log('fadeout 2');
-
-  //     $('body').css('overflow-y', 'scroll');
-  //   }
-  // };
-
-  $(window).on('load', () => {
-    console.log('hi');
-    // setVh();
+  window.addEventListener('resize', setVh);
+  window.onload = () => {
+    setVh();
     let nowLocation = window.location.href;
     if (nowLocation[nowLocation.length - 1] === '/') {
       $('.logoContainer').css('animation-iteration-count', '1');
@@ -64,14 +38,20 @@ function App() {
     } else {
       $('.logoLoading').fadeOut();
       console.log('fadeout 2');
+
       $('body').css('overflow-y', 'scroll');
     }
-  });
+  };
 
   const { token, setToken } = useToken();
   return (
     <BrowserRouter>
-      {/* <LogoLoading /> */}
+      {navigator.userAgent.indexOf('Safari') != -1 &&
+      navigator.userAgent.indexOf('Chrome') == -1 ? (
+        <></>
+      ) : (
+        <LogoLoading />
+      )}
       <Routes>
         <Route exact path="/" element={<About />} />
         <Route path="/journey" element={<Journey />} />
